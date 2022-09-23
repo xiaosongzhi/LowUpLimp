9e5f8fbe649aa51bc81a1feb08620d28457ae5e9
xiaosongzhi <672203508@qq.com>
2022-09-23 15:34:22 +0800

    游戏界面控制程序

--- Src/GameControl/gamedisplaypage.cpp
+++ Src/GameControl/gamedisplaypage.cpp
@@ -1,6 +1,7 @@
 #include "gamedisplaypage.h"
 #include "ui_gamedisplaypage.h"
 #include <QPropertyAnimation>
+#include <QButtonGroup>
 #include <QDebug>
 GameDisplayPage::GameDisplayPage(QWidget *parent) :
     QWidget(parent),
@@ -13,10 +14,39 @@ GameDisplayPage::GameDisplayPage(QWidget *parent) :
     m_leftAnimation->setDuration(1000);
     m_rightAnimation->setDuration(1000);
 
+    QButtonGroup *upLimpGroup = new QButtonGroup(this);
+    upLimpGroup->addButton(ui->upForward_Btn);
+    upLimpGroup->addButton(ui->upBackward_Btn);
+
+    QButtonGroup *downLimpGroup = new QButtonGroup(this);
+    downLimpGroup->addButton(ui->downBackward_Btn);
+    downLimpGroup->addButton(ui->downForward_Btn);
+
+    m_channelAList.append(ui->channelA1_Label);
+    m_channelAList.append(ui->channelA2_Label);
+    m_channelAList.append(ui->channelA3_Label);
+    m_channelAList.append(ui->channelA4_Label);
+    m_channelAList.append(ui->channelA5_Label);
+    m_channelAList.append(ui->channelA6_Label);
+    m_channelAList.append(ui->channelA7_Label);
+    m_channelAList.append(ui->channelA8_Label);
+
+    m_channelBList.append(ui->channelB1_Label);
+    m_channelBList.append(ui->channelB2_Label);
+    m_channelBList.append(ui->channelB3_Label);
+    m_channelBList.append(ui->channelB4_Label);
+    m_channelBList.append(ui->channelB5_Label);
+    m_channelBList.append(ui->channelB6_Label);
+    m_channelBList.append(ui->channelB7_Label);
+    m_channelBList.append(ui->channelB8_Label);
 }
 
 GameDisplayPage::~GameDisplayPage()
 {
+    if(m_leftAnimation)
+        delete m_leftAnimation;
+    if(m_rightAnimation)
+        delete m_rightAnimation;
     delete ui;
 }
 
@@ -27,10 +57,10 @@ void GameDisplayPage::on_start_Btn_clicked()
 
 void GameDisplayPage::open_Btn_clicked()
 {
-    m_leftAnimation->setStartValue(QPoint(-360,0));
+    m_leftAnimation->setStartValue(QPoint(-430,0));
     m_leftAnimation->setEndValue(QPoint(0,0));
     m_rightAnimation->setStartValue(QPoint(1920,0));
-    m_rightAnimation->setEndValue(QPoint(1560,0));
+    m_rightAnimation->setEndValue(QPoint(1490,0));
     m_leftAnimation->start(QAbstractAnimation::KeepWhenStopped);
     m_rightAnimation->start(QAbstractAnimation::KeepWhenStopped);
 }
@@ -38,8 +68,8 @@ void GameDisplayPage::open_Btn_clicked()
 void GameDisplayPage::close_Btn_clicked()
 {
     m_leftAnimation->setStartValue(QPoint(0,0));
-    m_leftAnimation->setEndValue(QPoint(-360,0));
-    m_rightAnimation->setStartValue(QPoint(1560,0));
+    m_leftAnimation->setEndValue(QPoint(-430,0));
+    m_rightAnimation->setStartValue(QPoint(1490,0));
     m_rightAnimation->setEndValue(QPoint(1920,0));
     m_leftAnimation->start(QAbstractAnimation::KeepWhenStopped);
     m_rightAnimation->start(QAbstractAnimation::KeepWhenStopped);
@@ -55,5 +85,132 @@ void GameDisplayPage::mouseDoubleClickEvent(QMouseEvent *event)
     m_openState = !m_openState;
 }
 
+void GameDisplayPage::on_upSpeedMinus_Btn_clicked()
+{
+    int speed = ui->upSpeed_Label->text().toInt();
+    if(speed > 2)
+    ui->upSpeed_Label->setText(QString::number(--speed));
+}
 
+void GameDisplayPage::on_upSpeedPlus_Btn_clicked()
+{
+    int speed = ui->upSpeed_Label->text().toInt();
+    if(speed < 60)
+    ui->upSpeed_Label->setText(QString::number(++speed));
+}
+
+void GameDisplayPage::on_upForceMinus_Btn_clicked()
+{
+    int force = ui->upForce_Label->text().toInt();
+    if(force > 0)
+        ui->upForce_Label->setText(QString::number(--force));
+}
+
+void GameDisplayPage::on_upForcePlus_Btn_clicked()
+{
+    int force = ui->upForce_Label->text().toInt();
+    if(force < 29)
+        ui->upForce_Label->setText(QString::number(++force));
+}
+
+void GameDisplayPage::on_upForward_Btn_clicked()
+{
+
+}
+
+void GameDisplayPage::on_upBackward_Btn_clicked()
+{
+
+}
+
+void GameDisplayPage::on_downSpeedMinus_Btn_clicked()
+{
+    int speed = ui->downSpeed_Label->text().toInt();
+    if(speed > 2)
+    ui->downSpeed_Label->setText(QString::number(--speed));
+}
+
+void GameDisplayPage::on_downSpeedPlus_Btn_clicked()
+{
+    int speed = ui->downSpeed_Label->text().toInt();
+    if(speed < 60)
+    ui->downSpeed_Label->setText(QString::number(++speed));
+}
+
+void GameDisplayPage::on_downForceMinus_Btn_clicked()
+{
+    int force = ui->downForce_Label->text().toInt();
+    if(force > 0)
+        ui->downForce_Label->setText(QString::number(--force));
+}
+
+void GameDisplayPage::on_downForcePlus_Btn_clicked()
+{
+    int force = ui->downForce_Label->text().toInt();
+    if(force < 29)
+        ui->downForce_Label->setText(QString::number(++force));
+}
+
+void GameDisplayPage::on_downForward_Btn_clicked()
+{
+
+}
+
+void GameDisplayPage::on_downBackward_Btn_clicked()
+{
+
+}
+
+void GameDisplayPage::setChannelAData(int *data,int size)
+{
+    if(size <= 8 )
+    for(int i = 0;i <size;i++)
+    {
+        m_channelAList.at(i)->setText(QString::number(data[i]));
+    }
+}
+
+void GameDisplayPage::setChannelBData(int *data,int size)
+{
+    if(size <= 8 )
+    for(int i = 0;i <size;i++)
+    {
+        m_channelBList.at(i)->setText(QString::number(data[i]));
+    }
+}
+
+void GameDisplayPage::setTrainSpeed(int speed, qint8 type)
+{
+    Q_UNUSED(speed)
+    Q_UNUSED(type)
+}
+
+void GameDisplayPage::setTrainFore(int force, qint8 type)
+{
+    Q_UNUSED(force)
+    Q_UNUSED(type)
+}
+
+void GameDisplayPage::setTrainDirection(qint8 direction, qint8 type)
+{
+    Q_UNUSED(direction)
+    Q_UNUSED(type)
+}
+
+void GameDisplayPage::switchFes(qint8 channel, bool ok)
+{
+    Q_UNUSED(channel)
+    Q_UNUSED(ok)
+}
+
+void GameDisplayPage::on_stop_Btn_clicked()
+{
+
+}
+
+
+void GameDisplayPage::on_pause_Btn_clicked()
+{
+
+}
 
--- Src/GameControl/gamedisplaypage.h
+++ Src/GameControl/gamedisplaypage.h
@@ -2,6 +2,7 @@
 #define GAMEDISPLAYPAGE_H
 
 #include <QWidget>
+#include <QLabel>
 class QPropertyAnimation;
 namespace Ui {
 class GameDisplayPage;
@@ -15,12 +16,70 @@ public:
     explicit GameDisplayPage(QWidget *parent = nullptr);
     ~GameDisplayPage();
 
+    void setChannelAData(int *data,int size);
+
+    void setChannelBData(int *data,int size);
+
+    /***********通信相关************/
+    /*****设置速度***
+     * 参数@int speed 速度大小
+     *    @qint8 type 上下肢类型 0-上肢  1-下肢
+     * ******/
+    void setTrainSpeed(int speed,qint8 type);
+
+    /******设置阻力****
+     * 参数@int force 阻力大小
+     *    @qint8 type  上下肢类型 0-上肢  1-下肢
+     * ********/
+    void setTrainFore(int force,qint8 type);
+
+    /******设置方向****
+     * 参数@qint8 direction 方向 0-顺时针  1-逆时针
+     *    @qint8 type  上下肢类型 0-上肢  1-下肢
+     * ********/
+    void setTrainDirection(qint8 direction,qint8 type);
+
+    /******设置Fes开关****
+     * 参数@qint8 channel 方向 0-电刺激A  1-电刺激B
+     *    @bool ok  上下肢类型 false-关  true-开
+     * ********/
+    void switchFes(qint8 channel,bool ok);
+
 private slots:
     void on_start_Btn_clicked();
 
     void open_Btn_clicked();
 
     void close_Btn_clicked();
+
+    void on_upSpeedMinus_Btn_clicked();
+
+    void on_upSpeedPlus_Btn_clicked();
+
+    void on_upForceMinus_Btn_clicked();
+
+    void on_upForcePlus_Btn_clicked();
+
+    void on_upForward_Btn_clicked();
+
+    void on_upBackward_Btn_clicked();
+
+    void on_downSpeedMinus_Btn_clicked();
+
+    void on_downSpeedPlus_Btn_clicked();
+
+    void on_downForceMinus_Btn_clicked();
+
+    void on_downForcePlus_Btn_clicked();
+
+    void on_downForward_Btn_clicked();
+
+    void on_downBackward_Btn_clicked();
+
+    void on_stop_Btn_clicked();
+
+    void on_pause_Btn_clicked();
+
 protected:
 
     virtual void mouseDoubleClickEvent(QMouseEvent *event);
@@ -29,6 +88,8 @@ private:
     Ui::GameDisplayPage *ui;
     QPropertyAnimation *m_leftAnimation,*m_rightAnimation;
     bool m_openState;
+    QList<QLabel*> m_channelAList,m_channelBList;
+
 };
 
 #endif // GAMEDISPLAYPAGE_H
