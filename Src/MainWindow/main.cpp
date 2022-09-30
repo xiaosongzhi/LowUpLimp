7a595a82ed5cf4f7faec5a346bdb92a6472a855b
xiaosongzhi <672203508@qq.com>
2022-09-30 13:29:15 +0800

    优化控件显示效果

--- Src/MainWindow/main.cpp
+++ Src/MainWindow/main.cpp
@@ -2,7 +2,9 @@
 #include <QApplication>
 #include "cdatabaseinterface.h"
 #include "mainwindow.h"
+#include <QFile>
 #include <QDebug>
+#include <QMessageBox>
 //#include "loginwidget.h"
 #include "cmainwindow.h"
 #include "gamedisplaypage.h"
@@ -13,7 +15,20 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     if(!CDatabaseInterface::getInstance()->openDB("./DependFile/DBFile/UpLow.db","QSQLITE"))
         qDebug()<<"UpLow.db open failed!";
-    MainWindow w;
+
+    //设置全局样式表
+    QFile file("./DependFile/QSS/app.txt");
+    if(file.open(QFile::ReadOnly))
+    {
+        QString styleSheet = QLatin1String(file.readAll());
+        qApp->setStyleSheet(styleSheet);
+        file.close();
+    }
+    else
+    {
+        QMessageBox::warning(NULL, "warning", "totalqss Open failed", QMessageBox::Yes | QMessageBox::No, QMessageBox::Yes);
+    }
+    CMainWindow w;
     w.show();
     return a.exec();
 }
