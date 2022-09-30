05ab9fb19d5dad5d5c697adf211563dd8b8c5acb
xiaosongzhi <672203508@qq.com>
2022-09-30 18:01:20 +0800

    添加登录窗口

--- Src/MainWindow/loginwidget.cpp
+++ Src/MainWindow/loginwidget.cpp
@@ -2,25 +2,41 @@
 #include "ui_loginwidget.h"
 #include <QSettings>
 #include <QDir>
+#include "passworddialog.h"
+#include <QDebug>
 LoginWidget::LoginWidget(QDialog *parent) :
     QDialog(parent),
-    ui(new Ui::LoginWidget)
+    ui(new Ui::LoginWidget),
+    passworldDialog(nullptr)
 {
     ui->setupUi(this);
-    setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
+    setWindowFlags(Qt::FramelessWindowHint);
     //设置为模态对话框
-    setModal(true);
+//    setModal(true);
     connect(this,&LoginWidget::signalResult,this,&LoginWidget::done);
+
+    ui->userNameTips_Label->setVisible(false);
+    ui->passwordTips_Label->setVisible(false);
+
+    passworldDialog = new PasswordDialog();
+
+    ui->password_LineEdit->setEchoMode(QLineEdit::Password);
+
+//    ui->password_LineEdit->setEchoMode(QLineEdit::PasswordEchoOnEdit);
 }
 
 LoginWidget::~LoginWidget()
 {
+    if(passworldDialog)
+        delete passworldDialog;
     delete ui;
 }
 
 void LoginWidget::on_forgetPassword_Btn_clicked()
 {
     //弹出提示框，告知获取密码的方式
+    passworldDialog->show();
+
 }
 
 void LoginWidget::on_confirm_Btn_clicked()
@@ -32,19 +48,36 @@ void LoginWidget::on_confirm_Btn_clicked()
     QString confFile(dirPath + "IDconf.ini");
     QSettings iniSetting(confFile, QSettings::IniFormat);
     QString password = iniSetting.value("password").toString();
-    QString userName = iniSetting.value("userName").toString();;
+    QString userName = iniSetting.value("userName").toString();
+
     if(ui->userName_LineEdit->text() != userName)
     {
+        ui->userNameTips_Label->setVisible(true);
         ui->userNameTips_Label->setText(tr("用户名输入错误"));
-//        return;
+        return;
     }
     if(ui->password_LineEdit->text() != password)
     {
+        ui->passwordTips_Label->setVisible(true);
         ui->passwordTips_Label->setText(tr("密码输入错误"));
-//        return;
+        return;
     }
     //设置返回值结果
     emit signalResult(1);
-    this->close();
+//    this->close();
+}
+
+
+void LoginWidget::on_userName_LineEdit_textChanged(const QString &arg1)
+{
+    Q_UNUSED(arg1)
+    ui->userNameTips_Label->setVisible(false);
+}
+
+
+void LoginWidget::on_password_LineEdit_textChanged(const QString &arg1)
+{
+    Q_UNUSED(arg1)
+    ui->passwordTips_Label->setVisible(false);
 }
 
--- Src/MainWindow/loginwidget.h
+++ Src/MainWindow/loginwidget.h
@@ -2,6 +2,7 @@
 #define LOGINWIDGET_H
 
 #include <QDialog>
+class PasswordDialog;
 namespace Ui {
 class LoginWidget;
 }
@@ -20,8 +21,13 @@ private slots:
 
     void on_confirm_Btn_clicked();
 
+    void on_userName_LineEdit_textChanged(const QString &arg1);
+
+    void on_password_LineEdit_textChanged(const QString &arg1);
+
 private:
     Ui::LoginWidget *ui;
+    PasswordDialog *passworldDialog;
 };
 
 #endif // LOGINWIDGET_H
