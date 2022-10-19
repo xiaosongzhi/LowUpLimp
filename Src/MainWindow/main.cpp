196ff8ee4ac2ef7125fab76e516caf3c9e549db6
xiaosongzhi <672203508@qq.com>
2022-10-19 18:02:41 +0800

    测试函数添加

--- Src/MainWindow/main.cpp
+++ Src/MainWindow/main.cpp
@@ -7,13 +7,15 @@
 //#include "loginwidget.h"
 #include "cmainwindow.h"
 #include "readconfig.h"
-
+#include "ccommunicateapi.h"
 #include "loginwidget.h"
 
 int main(int argc, char *argv[])
 {
+
     //    qputenv("QT_IM_MODULE",QByteArray("qtvirtualkeyboard"));
     QApplication a(argc, argv);
+
     //数据库读取
     if(!CDatabaseInterface::getInstance()->openDB("./DependFile/DBFile/UpLow.db","QSQLITE"))
         qDebug()<<"UpLow.db open failed!";
@@ -23,11 +25,6 @@ int main(int argc, char *argv[])
         qDebug()<<"配置文件读取失败";
         return -1;
     }
-    int16_t port;
-    QString IP;
-    ReadConfig::getInstance()->getGameServerAddress(port,IP);
-    qDebug()<<port<<IP;
-
 
     //设置全局样式表
     QFile file("./DependFile/QSS/app.txt");
@@ -46,6 +43,21 @@ int main(int argc, char *argv[])
     w.show();
     LoginWidget login;
     login.show();
+
+    ST_BicycleParam st_bicycleParam;
+    st_bicycleParam.controlState = 1;
+    st_bicycleParam.bodyPart =1;
+    st_bicycleParam.trainMode = 9;
+    st_bicycleParam.spasmSwitch = 1;
+    st_bicycleParam.spasmLevel = 2;
+    st_bicycleParam.configPower = 0;
+    st_bicycleParam.switchDirectonTime = 0;
+    st_bicycleParam.phaseValue = 9;
+    st_bicycleParam.direction = 1;
+    st_bicycleParam.speed = 5;
+    st_bicycleParam.resistance = 2;
+    st_bicycleParam.spasmType = 1;
+    CCommunicateAPI::getInstance()->sendBicycleParam(st_bicycleParam);
 
     return a.exec();
 }
